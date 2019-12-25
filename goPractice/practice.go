ac33a92332aa53283c40a85079e8f42cee920142
ZhuYu <neverlandkyo@gmail.com>
2019-12-25 16:43:28 +0800

    practice 1.7 1 webServer

--- goPractice/practice.go
+++ goPractice/practice.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("edit in master branch")
-	fmt.Println("first edit in git branch edit")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "one two switch")
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
