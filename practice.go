448c7eeb46122f02fc2c0e9d565f9e33b4c06fd0
朱禹 <zhuyu@zhuyudeMacBook-Pro.local>
2020-02-06 11:20:50 -0300

    2.5_1 try type definition

--- practice.go
+++ practice.go
@@ -1,7 +1,27 @@
 package main
 
+//import "fmt"
+//
+//func main() {
+//    fmt.Println("show me the code")
+//}
+
+//practice 2.5 1
 import "fmt"
 
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
 func main() {
-    fmt.Println("show me the code")
+	var currentTemp Celsius = 19
+	fmt.Printf("current temperature is %g Celsius, %g Fahrenheit.", currentTemp, CToF(currentTemp))
 }
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
